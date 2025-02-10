daf0aa634b5fcffc68273d9f5b5b82b34cc5e83e
Onyishi James <jammykrix@gmail.com>
2025-02-10 08:46:28 +0100

    Update settings.py

--- hngXII0/settings.py
+++ hngXII0/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ["https://hng12stage0-e2hb.onrender.com", "*"]
+ALLOWED_HOSTS = ["hng12stage0-production-4792.up.railway.app", "*"]
 
 
 # Application definition
